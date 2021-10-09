3dda4bd22972b07a37df2f34f5217f517c37acbc
Tokenizers <romain.barraud1@gmail.com>
2021-10-09 09:35:56 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "gridvsflexbox",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-react-helmet",
+  ],
+};
